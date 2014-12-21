fea6e6cd0691b3a83e842cbcacd98ebc0c41f722
Jason Wagner <jaypwagner@gmail.com>
2014-12-20 23:37:47 -0600

    baseline for .NET GIS activities

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SharpMappingWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapBox1 = new SharpMap.Forms.MapBox();
+            this.SuspendLayout();
+            // 
+            // mapBox1
+            // 
+            this.mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.None;
+            this.mapBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapBox1.BackColor = System.Drawing.Color.White;
+            this.mapBox1.FineZoomFactor = 10D;
+            this.mapBox1.Location = new System.Drawing.Point(12, 12);
+            this.mapBox1.MapQueryMode = SharpMap.Forms.MapBox.MapQueryType.LayerByIndex;
+            this.mapBox1.Name = "mapBox1";
+            this.mapBox1.QueryGrowFactor = 5F;
+            this.mapBox1.QueryLayerIndex = 0;
+            this.mapBox1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.ShowProgressUpdate = false;
+            this.mapBox1.Size = new System.Drawing.Size(391, 347);
+            this.mapBox1.TabIndex = 0;
+            this.mapBox1.Text = "mapBox1";
+            this.mapBox1.WheelZoomMagnitude = -2D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 375);
+            this.Controls.Add(this.mapBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpMap.Forms.MapBox mapBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using SharpMap.Styles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BruTile;
+using ProjNet;
+using ProjNet.CoordinateSystems;
+namespace SharpMappingWinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string tutFile = @"D:\states_ugl.shp";
+            string worldBorders = @"D:\BordersProject\TM_WORLD_BORDERS-0.3.shp";
+            List<string> texasShapeFiles = new List<string>();
+            texasShapeFiles.Add(@"D:\BordersProject\Texas\buildings.shp");
+            texasShapeFiles.Add(@"D:\BordersProject\Texas\landuse.shp");
+            texasShapeFiles.Add(@"D:\BordersProject\Texas\natural.shp");
+            texasShapeFiles.Add(@"D:\BordersProject\Texas\places.shp");
+            texasShapeFiles.Add(@"D:\BordersProject\Texas\points.shp");
+            texasShapeFiles.Add(@"D:\BordersProject\Texas\railways.shp");
+            texasShapeFiles.Add(@"D:\BordersProject\Texas\roads.shp");
+            texasShapeFiles.Add(@"D:\BordersProject\Texas\waterways.shp");
+
+            SharpMap.Layers.VectorLayer vlay = new SharpMap.Layers.VectorLayer("States");
+            SharpMap.Layers.VectorLayer vlay2 = new SharpMap.Layers.VectorLayer("world");
+            SharpMap.Layers.VectorLayer vlayTx = new SharpMap.Layers.VectorLayer("texas");
+
+            vlay.DataSource = new SharpMap.Data.Providers.ShapeFile(tutFile, true);
+            vlay.DataSource = new SharpMap.Data.Providers.ShapeFile(worldBorders, true);
+           
+            vlayTx.DataSource = new SharpMap.Data.Providers.ShapeFile(@"D:\BordersProject\Texas\natural.shp");
+            foreach (var s in texasShapeFiles)
+            {
+                //vlayTx.DataSource = new SharpMap.Data.Providers.ShapeFile(s, true);
+            }
+            //SharpMap.Layers.VectorLayer vlay = new SharpMap.Layers.VectorLayer("States");
+            //vlay.DataSource = new SharpMap.Data.Providers.ShapeFile(worldBorders, true);
+
+            //Create the style for Land
+            VectorStyle defaultStyle = new VectorStyle();
+
+            //Create the style for Water
+            VectorStyle africa = new VectorStyle();
+            VectorStyle antartica = new VectorStyle();
+            VectorStyle australia = new VectorStyle();
+            VectorStyle northAmerica = new VectorStyle();
+            VectorStyle asia = new VectorStyle();
+            VectorStyle europe = new VectorStyle();
+            // Set Random Colors
+            Random rnd = new Random();
+
+            ////Create the map
+            Dictionary<int, IStyle> styles = new Dictionary<int, IStyle>();
+            styles.Add(999, defaultStyle);
+            styles.Add(0, antartica);
+            styles.Add(2, africa);
+            styles.Add(9, australia);
+            styles.Add(19, northAmerica);
+            styles.Add(142, asia);
+            styles.Add(150, europe);
+
+            foreach (KeyValuePair<int, IStyle> style in styles)
+            {
+                var o = style.Value as VectorStyle;
+                o.EnableOutline = true;
+                rnd.Next(0, 256);
+
+                var r = rnd.Next(0, 256);
+                var g = rnd.Next(0, 256);
+                var b = rnd.Next(0, 256);
+                o.Fill = new SolidBrush(Color.FromArgb(r, g, b));
+                //.EnableOutline = true;
+            }
+
+            //Assign the theme
+            vlay.Theme = new SharpMap.Rendering.Thematics.UniqueValuesTheme<int>("region", styles, defaultStyle);
+            //GeoAPI.Geometries.Envelope i = vlayTx.DataSource.GetGeometriesInView(mapBox1.Bounds);
+            //vlayTx.Enabled = true;
+            mapBox1.Map.Layers.Add(vlay);
+
+            SharpMap.Layers.WmsLayer wmsL =
+               new SharpMap.Layers.WmsLayer("US Cities",
+                   "http://sampleserver1.arcgisonline.com/ArcGIS/services/Specialty/ESRI_StatesCitiesRivers_USA/MapServer/WMSServer");
+            //Force PNG format. Else we can't see through
+            wmsL.SetImageFormat("image/png");
+            //Force version 1.1.0
+            wmsL.Version = "1.1.0";
+            //Add layer named 2 in the service (Cities)
+            wmsL.AddLayer("2");
+            //Set the SRID
+            wmsL.SRID = 4326;
+            mapBox1.Map.Layers.Add(wmsL);
+
+            // mapBox1.Map.Layers.Add(vlay);
+            mapBox1.Map.ZoomToExtents();
+            mapBox1.Refresh();
+            mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.Pan;
+        }
+
+
+
+    }
+}
